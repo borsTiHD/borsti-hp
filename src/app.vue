6767d2a00c0ba27337c9bff8efd1eb77b6e74e7a
borsTiHD <bastian.j@gmx.net>
2022-04-03 23:22:12 +0200

    💄 App >> Changing bg styling.

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="bg-gray-900 w-full min-h-screen">
+    <div class="w-full min-h-screen bg-gray-100 dark:bg-gray-900 bg-fixed bg-cover bg-bottom bg-blend-luminosity bg-image">
         <NuxtPage />
     </div>
 </template>
+
+<style scope>
+	.bg-image {
+		/* background-image: url("assets/img/coding.jpg") */
+	}
+</style>
