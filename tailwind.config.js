a4919f23829058f363a14642f7edbf96a9f844f1
borsTiHD <bastian.j@gmx.net>
2022-04-05 18:33:13 +0200

    💄 App >> Changing primary color a little...

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
 				// https://coolors.co/palette/e63946-f1faee-a8dadc-457b9d-1d3557
 				// https://coolors.co/palette/780000-c1121f-fdf0d5-003049-669bbc
 				// https://coolors.co/palette/2b2d42-8d99ae-edf2f4-ef233c-d90429
-				primary: '#EF233C',
+				primary: '#FF2943', // #FF2943 - #EF233C
 				'primary-light': '#D90429',
 				secondary: '#EDF2F4',
 				'bg-light': '#8D99AE',
