0523fc0d5d8c6813dcd669807b5b3c1284f93043
borsTiHD <bastian.j@gmx.net>
2022-04-05 18:08:03 +0200

    💄 App >> New color set

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
 		extend: {
 			colors: {
 				// https://coolors.co/palette/e63946-f1faee-a8dadc-457b9d-1d3557
-				primary: '#E63946',
-				secondary: '#F1FAEE',
-				'bg-light': '#A8DADC',
-				bg: '#457B9D',
-				'bg-dark': '#1D3557'
+				// https://coolors.co/palette/780000-c1121f-fdf0d5-003049-669bbc
+				// https://coolors.co/palette/2b2d42-8d99ae-edf2f4-ef233c-d90429
+				primary: '#EF233C',
+				'primary-light': '#D90429',
+				secondary: '#EDF2F4',
+				'bg-light': '#8D99AE',
+				bg: '#2B2D42'
 			}
 		}
 	},
