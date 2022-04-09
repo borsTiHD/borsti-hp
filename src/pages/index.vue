82e7f0a51317d600a83f5d1ef7cf7f128f2c1863
borsTiHD <bastian.j@gmx.net>
2022-04-09 12:55:43 +0200

    🎨 Home >> Changing the hole construct for creating text. Also added a small set of functions for creating skillset decoration.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,25 +8,53 @@
 			<div class="border-t lg:border-t-0 border-gray-400 dark:border-gray-500 pt-6 lg:pt-0" />
 			<!-- Introduction -->
 			<blockquote>
-				<p class="text-primary-light dark:text-primary font-montserrat font-bold my-5 text-lg lg:text-xl xl:text-2xl leading-tight uppercase">{{ introduction }}</p>
-				<h1 class="text-gray-900 dark:text-white border-l-2 border-primary drop-shadow-md font-roboto text-6xl lg:text-7xl xl:text-8xl leading-none pl-6">{{ name }}</h1>
-				<h2 class="text-gray-900 dark:text-white font-montserrat my-10 text-xl lg:text-2xl xl:text-3xl leading-tight">{{ underline }}</h2>
+				<p class="text-primary-light dark:text-primary font-montserrat font-bold my-5 text-lg lg:text-xl xl:text-2xl leading-tight uppercase" v-text="introduction" />
+				<h1 class="text-gray-900 dark:text-white border-l-2 border-primary drop-shadow-md font-roboto text-6xl lg:text-7xl xl:text-8xl leading-none pl-6" v-text="name" />
+				<h2 class="text-gray-900 dark:text-white font-montserrat my-10 text-xl lg:text-2xl xl:text-3xl leading-tight" v-text="underline" />
 			</blockquote>
 			<!-- About me -->
 			<blockquote class="backdrop-blur-md bg-white/10 dark:bg-white/10 p-2 mb-4">
-				<p class="text-gray-900 dark:text-white font-montserrat text-base md:text-md leading-relaxed max-w-prose m-2 mb-6">{{ description }}</p>
-				<p class="text-gray-900 dark:text-white font-montserrat text-base md:text-md leading-relaxed max-w-prose m-2 mt-6">{{ skillset }}</p>
+				<p class="text-gray-900 dark:text-white font-montserrat text-base md:text-md leading-relaxed max-w-prose m-2 mb-6" v-html="description" />
+				<p class="text-gray-900 dark:text-white font-montserrat text-base md:text-md leading-relaxed max-w-prose m-2 mt-6" v-html="skillset" />
 			</blockquote>
 		</figcaption>
 	</figure>
 </template>
 
 <script setup>
+// Skils for building skillset
+const skills = [
+	'design',
+	'NodeJS',
+	'Vue',
+	'Electron',
+	'PHP',
+	'MySQL',
+	'Linux server administration'
+]
+
+// Random color for skillset
+const getRandomColor = () => {
+	const colors = [
+		'decoration-sky-500/60',
+		'decoration-teal-500/60',
+		'decoration-primary/60'
+	]
+	return colors[Math.floor(Math.random() * colors.length)]
+}
+
+// Creates skill tag
+const getSkill = (value, skill = undefined, color = undefined) => {
+	return `<a class="font-roboto italic underline underline-offset-2 ${color || getRandomColor()} decoration-2">${skill || skills[value]}</a>`
+}
+
+// Building texts
+let counter = 0
 const introduction = 'Hello, my name is'
 const name = 'Bastian Jakobs'
-const underline = 'Creator • YouTuber • GFX Designer • Artist'
-const description = 'I\'m a full-stack web developer from Germany, working as a software engineer at Deutsche Telekom. I have the passion to make the web a beautiful place.'
-const skillset = 'Building web and desktop apps since school. I have a diverse set of skills, ranging from design, NodeJS, Vue, Electron all the way to PHP, MySQL and Linux server administration.'
+const underline = 'Creator • YouTuber • GFX • Developer'
+const description = `I'm a full-stack web developer from Germany, working as a software engineer at ${getSkill(false, 'Deutsche Telekom', 'decoration-magenta/80')}. I have the passion to make the web a beautiful place.`
+const skillset = `Building web and desktop apps since school. I have a diverse set of skills, ranging from ${getSkill(counter++)}, ${getSkill(counter++)}, ${getSkill(counter++)}, ${getSkill(counter++)} all the way to ${getSkill(counter++)}, ${getSkill(counter++)} and ${getSkill(counter++)}.`
 </script>
 
 <style scope>
