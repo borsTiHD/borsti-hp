b6524799b09d808c9b2b20c8dc7f92c53f851f06
borsTiHD <bastian.j@gmx.net>
2022-04-11 09:20:53 +0200

    🗃️ DarkMode >> Changing state syntax.

--- src/store/darkmode.ts
+++ src/store/darkmode.ts
@@ -2,11 +2,9 @@ import { defineStore } from 'pinia'
 
 export const useDarkModeStore = defineStore({
 	id: 'darkmode-store',
-	state: () => {
-		return {
-			darkMode: true
-		}
-	},
+	state: () => ({
+		darkMode: true
+	}),
 	actions: {
 		setDarkMode(value: boolean) {
 			this.darkMode = value
