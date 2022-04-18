06df0c5895f5dc0701d0168915d8882debefcbdd
borsTiHD <bastian.j@gmx.net>
2022-04-18 10:48:27 +0200

    🎨 Card >> Extend component for header slot.

--- src/components/misc/AppCard.vue
+++ src/components/misc/AppCard.vue
@@ -1,5 +1,19 @@
 <template>
 	<div class="bg-black/10 dark:bg-white/10 rounded-lg p-4 my-4">
+		<!-- Header - only renders if slot is given -->
+		<h1 v-if="hasHeaderSlot">
+			<slot name="header" />
+		</h1>
+
+		<!-- Content - Default slot -->
 		<slot />
 	</div>
 </template>
+
+<script setup>
+import { useSlots } from 'vue'
+const slots = useSlots()
+
+// Test if has header slot
+const hasHeaderSlot = !!slots.header
+</script>
