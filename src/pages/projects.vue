610526f75815b16ce294eb29889d7e20fcf09fd0
borsTiHD <bastian.j@gmx.net>
2022-04-21 21:12:14 +0200

    🎨 Projects >> Using store data and routing to upcoming project page.

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -11,20 +11,21 @@
 				<AppCard
 					v-for="(project, index) in projects"
 					:key="index"
-					:img="project.img"
+					:img="project.preview"
+					image-hover
 				>
-					<template #header>{{ project.title }}</template>
+					<template #header>{{ project.name }}</template>
 					<template #default>
 						<div class="flex flex-col">
 							<AppDivider class="mt-0" />
-							<p class="font-roboto" v-text="project.description" />
+							<p class="font-roboto" v-text="project.introduction" />
 						</div>
 					</template>
 					<template #footer>
 						<div class="flex flex-row gap-2">
 							<span>Tags:</span>
 							<span>{{ project.topics.join(', ') }}</span>
-							<AppButton class="ml-auto" size="small" uppercase @click="openUrl(project.url)">See more</AppButton>
+							<AppButton class="ml-auto" size="small" uppercase @click="openProject(project.name)">See more</AppButton>
 						</div>
 					</template>
 				</AppCard>
@@ -38,25 +39,19 @@ import AppDivider from '@/components/misc/AppDivider.vue'
 import AppButton from '@/components/misc/AppButton.vue'
 import AppCard from '@/components/misc/AppCard.vue'
 import { useAppStore } from '~/store/app'
+import { useProjectsStore } from '~/store/projects'
 
-// Changing Title
+// Changing title
 const appStore = useAppStore()
 const pageTitle = `Projects - ${appStore.getTitle}`
 useHead({ title: pageTitle })
 
-// Open url in new tab
-const openUrl = (url) => {
-	window.open(url, '_blank')
+// Open project
+const openProject = (name) => {
+	navigateTo({ path: `/project/${name}` })
 }
 
-const ipsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
-
-// Projects
-const projects = [...Array(10).keys()].map((i) => ({
-	title: `Project ${i + 1}`,
-	img: `https://picsum.photos/id/${i + 1}/860/860`,
-	url: 'https://github.com/borsTiHD',
-	topics: ['vue', 'nuxt', 'express', 'jws', 'socket.io'],
-	description: ipsum
-}))
+// Get all projects
+const projectsStore = useProjectsStore()
+const projects = projectsStore.getProjects
 </script>
