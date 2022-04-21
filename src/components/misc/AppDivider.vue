69ddd7d98d61bb6f18a6fc6dcc3fc56f4437fb81
borsTiHD <bastian.j@gmx.net>
2022-04-21 19:10:06 +0200

    ✨ AppDivider >> New component for creating a divider.

---
+++ src/components/misc/AppDivider.vue
@@ -0,0 +1,23 @@
+
+<template>
+	<div
+		:class="[
+			'border-t pt-2 mt-2',
+			colorSets[props.colorSet],
+		]"
+	/>
+</template>
+
+<script setup>
+const colorSets = {
+	default: 'border-gray-400 dark:border-gray-500'
+}
+
+// Props
+const props = defineProps({
+	colorSet: {
+		type: String,
+		default: 'default'
+	}
+})
+</script>
