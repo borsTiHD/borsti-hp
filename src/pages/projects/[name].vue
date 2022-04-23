0260938e20ac64a427db1c3471c12696bb015b9c
borsTiHD <bastian.j@gmx.net>
2022-04-23 11:14:01 +0200

    ✨ Projects >> First testing the new modal component by clicking on a image. And a few ui changes to the component.

--- src/pages/projects/[name].vue
+++ src/pages/projects/[name].vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="flex flex-col text-gray-900 dark:text-white">
+		<AppModal v-model="showModal" />
 		<div class="flex justify-center">
 			<div class="my-4">
 				<h1 class="text-gray-900 dark:text-white text-5xl font-bold self-center">{{ project.name }}</h1>
@@ -8,11 +9,11 @@
 		</div>
 		<section class="flex justify-center bg-white dark:bg-body-bg p-8">
 			<div class="container mx-auto">
-				<AppCard :img="project.preview">
+				<AppCard>
 					<template #default>
 						<AppDivider class="mt-0" />
 						<div class="relative overflow-auto">
-							<img class="float-right m-2 h-48 object-cover rounded-lg drop-shadow-lg" :src="project.preview" loading="lazy">
+							<img class="float-right m-4 h-96 object-cover rounded-lg drop-shadow-lg" :src="project.preview" loading="lazy">
 							<p class="text-justify">{{ project.introduction + project.introduction + project.introduction + project.introduction }}</p>
 						</div>
 						<AppDivider />
@@ -22,8 +23,9 @@
 								:key="index"
 								:src="img"
 								height="100%"
-								class="card-image w-full h-48 object-cover rounded-lg drop-shadow-lg transform transition duration-500 hover:object-scale-down hover:scale-150 hover:h-full hover:z-50"
+								class="card-image w-full h-48 object-cover rounded-lg drop-shadow-lg cursor-pointer transform transition duration-500 hover:object-scale-down hover:scale-150 hover:z-10"
 								loading="lazy"
+								@click="openImage(img)"
 							>
 						</div>
 					</template>
@@ -31,7 +33,7 @@
 						<div class="flex flex-row gap-2">
 							<span>Tags:</span>
 							<span>{{ project.topics.join(', ') }}</span>
-							<AppButton class="ml-auto" size="small" uppercase @click="openProject(project.name)">See more</AppButton>
+							<AppButton class="ml-auto" uppercase @click="openProject(project.name)">See more</AppButton>
 						</div>
 					</template>
 				</AppCard>
@@ -41,6 +43,7 @@
 </template>
 
 <script setup>
+import AppModal from '@/components/layout/AppModal.vue'
 import AppDivider from '@/components/misc/AppDivider.vue'
 import AppButton from '@/components/misc/AppButton.vue'
 import AppCard from '@/components/misc/AppCard.vue'
@@ -59,4 +62,11 @@ const project = projectsStore.getProjectByName(name)
 const appStore = useAppStore()
 const pageTitle = `${project.name} - ${appStore.getTitle}`
 useHead({ title: pageTitle })
+
+// Open image in modal
+const showModal = ref(false)
+const openImage = (src) => {
+	console.log(src)
+	showModal.value = true
+}
 </script>
