a67f79ed8a8a3ef4210c7a92d11903d483e1c225
borsTiHD <bastian.j@gmx.net>
2022-04-24 11:11:02 +0200

    💄 AppModal >> Adding a hint for fullscreen and an exit button to the top if it is a image only modal.

--- src/components/layout/AppModal.vue
+++ src/components/layout/AppModal.vue
@@ -4,7 +4,13 @@
 		<!-- Modal container -->
 		<div class="modal-container p-4 w-full max-w-2xl m-auto">
 			<!-- Image Modal -->
-			<div v-if="image">
+			<div v-if="image" class="flex flex-col">
+				<div class="flex flex-row my-4">
+					<span class="text-white">Click image for fullscreen</span>
+					<button type="button" class="bg-neutral-800 hover:bg-neutral-400 text-gray-400 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" @click="closeModal">
+						<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
+					</button>
+				</div>
 				<img :src="image" class="w-full h-auto object-cover rounded-lg drop-shadow-lg cursor-pointer" loading="lazy" @click="openUrl(image)">
 			</div>
 
