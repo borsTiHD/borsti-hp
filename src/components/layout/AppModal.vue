07f2d46a86fb6387f60b0342b4918e19235003c1
borsTiHD <bastian.j@gmx.net>
2022-04-25 17:41:36 +0200

    💄 AppModal >> Optimising the structure. Image sizing is improved for image only modals.

--- src/components/layout/AppModal.vue
+++ src/components/layout/AppModal.vue
@@ -1,21 +1,22 @@
 <template>
 	<!-- Modal -->
 	<div v-show="open" tabindex="-1" aria-hidden="true" class="modal fixed inset-0 w-full h-screen bg-black/60 dark:bg-black/70 flex z-50" @click.self="closeModal">
-		<!-- Modal container -->
-		<div class="modal-container p-4 w-full max-w-2xl m-auto">
-			<!-- Image Modal -->
-			<div v-if="image" class="flex flex-col">
+		<!-- Image Modal -->
+		<div v-if="image" class="modal-container p-4 w-fit h-fit max-w-[90%] max-h-[90%] m-auto">
+			<div class="flex flex-col">
 				<div class="flex flex-row my-4" @click="closeModal">
 					<span class="text-white">Click image for fullscreen</span>
 					<button type="button" class="bg-neutral-800 hover:bg-neutral-400 text-gray-400 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" @click="closeModal">
 						<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
 					</button>
 				</div>
-				<img :src="image" class="w-full h-auto object-cover rounded-lg drop-shadow-lg cursor-pointer" loading="lazy" @click="openUrl(image)">
+				<img :src="image" class="object-contain rounded-lg drop-shadow-lg cursor-pointer" loading="lazy" @click="openUrl(image)">
 			</div>
+		</div>
 
-			<!-- Modal body -->
-			<div v-else class="modal-body relative rounded-lg shadow bg-neutral-100 dark:bg-neutral-800">
+		<!-- Text Modal -->
+		<div v-else class="modal-container p-4 w-full max-w-2xl m-auto">
+			<div class="modal-body relative rounded-lg shadow bg-neutral-100 dark:bg-neutral-800">
 				<!-- Modal header -->
 				<div v-if="hasHeaderSlot" class="modal-header flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
 					<h3 class="text-xl lg:text-2xl font-semibold text-gray-900 dark:text-white">
