48b4c68036908ec15ba6f8c34563e89b02d6df4a
borsTiHD <bastian.j@gmx.net>
2022-05-16 12:16:05 +0200

    💄 Modal >> Better preview for a image and better visual for the "click" hint.

--- src/components/misc/AppModal.vue
+++ src/components/misc/AppModal.vue
@@ -5,12 +5,12 @@
 		<div v-if="image" class="modal-container p-4 w-fit h-fit max-w-[90%] max-h-[90%] m-auto">
 			<div class="flex flex-col">
 				<div class="flex flex-row my-4" @click="closeModal">
-					<span class="text-white">Click image for fullscreen</span>
+					<span class="bg-neutral-800 hover:bg-neutral-400 text-white cursor-pointer rounded-lg p-1.5 px-2" @click="openUrl(image)">Click image for fullscreen</span>
 					<button type="button" class="bg-neutral-800 hover:bg-neutral-400 text-gray-400 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" @click="closeModal">
 						<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
 					</button>
 				</div>
-				<img :src="image" class="object-contain rounded-lg drop-shadow-lg cursor-pointer" loading="lazy" @click="openUrl(image)">
+				<img :src="image" class="object-contain rounded-lg drop-shadow-lg cursor-pointer max-h-[48rem]" loading="lazy" @click="openUrl(image)">
 			</div>
 		</div>
 
