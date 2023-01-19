8d8de770c338dc0ee4d15d3bbad78a38fbba95a1
borsTiHD <bastian.j@gmx.net>
2023-01-19 14:36:51 +0100

    💄 Navbar >> Redesign.

--- src/components/layout/AppNavbar.vue
+++ src/components/layout/AppNavbar.vue
@@ -1,38 +1,48 @@
 <template>
-	<nav class="navbar ml-auto m-4 gap-2 rounded-lg bg-base-300/40 w-fit">
-		<div class="md:flex hidden flex-1">
-			<a class="btn btn-ghost normal-case text-xl">borsTiHD</a>
-		</div>
-		<div class="flex-none gap-2">
-			<ul class="menu menu-horizontal px-1">
-				<NuxtLink v-for="(item, index) in navItems" :key="index" :to="item.to">
-					<li><a>{{ item.name }}</a></li>
-				</NuxtLink>
-			</ul>
-			<div class="dropdown dropdown-end">
-				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-					<div class="w-10 rounded-full">
-						<img src="https://placeimg.com/80/80/people">
-					</div>
+	<nav class="navbar md:ml-auto md:m-4 gap-2 rounded-lg bg-base-300/40 md:w-fit">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<label tabindex="0" class="btn btn-ghost lg:hidden">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 				</label>
-				<div class="mt-3 p-2 gap-2 shadow menu menu-compact dropdown-content bg-base-300/80 rounded-box w-52">
-					<ul tabindex="0">
-						<li>    
-							<a class="justify-between">
-								Profile
-								<span class="badge">New</span>
-							</a>
-						</li>
-					</ul>
-					<div class="form-control w-full max-w-xs">
-						<label class="label">
-							<span class="label-text">Select theme</span>
-						</label>
-						<select v-model="colorMode.preference" class="select select-sm select-info">
-							<option disabled selected>Theme</option>
-							<option v-for="theme of themes" :key="theme">{{ theme }}</option>
-						</select>
-					</div>
+				<ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
+					<li v-for="(item, index) in navItems" :id="index" :key="index">
+						<NuxtLink :to="item.to">{{ item.name }}</NuxtLink>
+					</li>
+				</ul>
+			</div>
+			<NuxtLink :to="navItems[0].to" class="btn btn-ghost normal-case text-xl">{{ appName }}</NuxtLink>
+		</div>
+		<div class="hidden md:flex flex-none gap-2">
+			<ul class="menu menu-horizontal px-1">
+				<li v-for="(item, index) in navItems" :id="index" :key="index">
+					<NuxtLink :to="item.to">{{ item.name }}</NuxtLink>
+				</li>
+			</ul>
+		</div>
+		<div class="ml-auto dropdown dropdown-end">
+			<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					<img src="https://placeimg.com/80/80/people">
+				</div>
+			</label>
+			<div class="menu menu-compact dropdown-content mt-3 p-2 gap-2 shadow bg-base-300 rounded-box w-52">
+				<ul tabindex="0">
+					<li>    
+						<a class="justify-between">
+							Profile
+							<span class="badge">New</span>
+						</a>
+					</li>
+				</ul>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Select theme</span>
+					</label>
+					<select v-model="colorMode.preference" class="select select-sm select-info">
+						<option disabled selected>Theme</option>
+						<option v-for="theme of themes" :key="theme">{{ theme }}</option>
+					</select>
 				</div>
 			</div>
 		</div>
@@ -41,6 +51,8 @@
 
 <script setup>
 import { usePagesStore } from '~/store/pages'
+
+const appName = 'borsTiHD'
 
 // Navitems
 const pagesStore = usePagesStore()
