7d4da845890991f860327af36c817a8820228760
borsTiHD <bastian.j@gmx.net>
2023-01-19 10:23:06 +0100

    ➕ App >> Adding vite-plugin-eslint.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
+import eslintPlugin from 'vite-plugin-eslint'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    srcDir: 'src/',
+    runtimeConfig: {
+        // The private keys which are only available within server-side
+        TWITTER_KEY: process.env.TWITTER_KEY || '',
+        TWITTER_SECRET: process.env.TWITTER_SECRET || '',
+        // Keys within public, will be also exposed to the client-side
+        public: {
+            publicKey: '123'
+        }
+    },
     modules: [
         '@nuxtjs/tailwindcss',
         '@pinia/nuxt'
-    ]
+    ],
+    vite: {
+        plugins: [
+            eslintPlugin()
+        ]
+    }
 })
