55e112aea52638a36934a4bfe4da7d40fa95b760
borsTiHD <bastian.j@gmx.net>
2023-01-19 16:24:56 +0100

    ✨ App >> Adding first page for content.

--- src/app.vue
+++ src/app.vue
@@ -1,28 +1,9 @@
 <template>
 	<div class="bg-fixed bg-top bg-repeat-x bg-scales-small md:bg-scales bg-[length:auto_140%] flex flex-col min-h-screen" :class="[themeMode]">
 		<AppNavbar />
-		<div class="hero min-h-screen bg-base-400 py-8 pt-32 -mt-24">
-			<div class="hero-content flex-col lg:flex-row">
-				<img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl">
-				<div>
-					<h1 class="text-5xl font-bold">Box Office News!</h1>
-					<p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-					<button class="btn btn-primary">Get Started</button>
-				</div>
-			</div>
-		</div>
-		<div class="p-10">
-			<div class="card w-96 bg-base-100 shadow-xl">
-				<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes"></figure>
-				<div class="card-body">
-					<h2 class="card-title">Shoes!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Buy Now</button>
-					</div>
-				</div>
-			</div>
-		</div>
+		<main>
+			<NuxtPage />
+		</main>
 	</div>
 </template>
 
@@ -100,12 +81,5 @@ const themeMode = computed(() => {
 		0% { background-position-x:10% }
 		50% { background-position-x:0% }
 		to { background-position-x:10% }
-	}
-
-	@keyframes shine {
-		to {
-			background-position: 100% 0, 0 0, 0 40px, /* moves the highlight to the right */
-			0 80px, 0 120px;
-		}
 	}
 </style>
---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="-mt-24">
+		<section class="hero min-h-screen bg-base-400">
+			<div class="hero-content flex-col lg:flex-row">
+				<img src="~/assets/img/borsti_bw.png" :alt="`Photo of ${name}`" class="max-w-sm rounded-lg drop-shadow-xl">
+				<div>
+					<h1 class="text-5xl font-bold">Box Office News!</h1>
+					<p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+					<button class="btn btn-primary">Get Started</button>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="p-10">
+				<div class="card w-96 bg-base-100 shadow-xl">
+					<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes"></figure>
+					<div class="card-body">
+						<h2 class="card-title">Shoes!</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+						<div class="card-actions justify-end">
+							<button class="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script setup>
+const name = 'Bastian Jakobs'
+</script>
