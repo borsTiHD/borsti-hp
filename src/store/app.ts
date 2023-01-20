a966a522558f6f10ee11145bc988c56c94155ed4
borsTiHD <bastian.j@gmx.net>
2023-01-20 19:05:51 +0100

    ✨ App >> Adding author to app-store.

--- src/store/app.ts
+++ src/store/app.ts
@@ -3,9 +3,11 @@ import { defineStore } from 'pinia'
 export const useAppStore = defineStore({
     id: 'app-store',
     state: () => ({
-        headTitle: 'borstihd.de'
+        headTitle: 'borstihd.de',
+        author: 'Bastian Jakobs'
     }),
     getters: {
-        getTitle: (state) => state.headTitle
+        getTitle: (state) => state.headTitle,
+        getAuthor: (state) => state.author
     }
 })
